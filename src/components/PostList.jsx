6297970987fbdc1283f9e83579da163667b971e9
ChetanAnilMore <chetanmore733@gmail.com>
2024-04-08 15:31:16 +0530

    Added Basic Components of a Social media app

---
+++ src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import Post from "./Post";
+
+const PostList = () => {
+  return (
+    <div className="posts">
+      <Post className="post"></Post>
+      <Post className="post"></Post>
+      <Post className="post"></Post>
+      <Post className="post"></Post>
+    </div>
+  );
+};
+
+export default PostList;
